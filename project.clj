ef8718a28335989d180485b6efb0376a9144193a
Brian Picciano <bgp2396@bellsouth.net>
2012-12-08 13:57:23 -0500

    got meta parsing strings into meta structs

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojars.mediocregopher/massage "0.1.5"]])
+                 [org.clojars.mediocregopher/massage "0.1.5"]
+                 [cheshire "5.0.1"]])
--- src/duffel/meta.clj
+++ src/duffel/meta.clj
@@ -1,5 +1,7 @@
 (ns duffel.meta
-   (:import java.lang.System))
+    (:require [cheshire.core :refer :all])
+    (:use massage.json)
+    (:import java.lang.System))
 
 (def default-username (java.lang.System/getProperty "user.name"))
 (def default-group default-username)    ;Assume primary group = username, cause fuck it
@@ -12,3 +14,27 @@
                    { :apply_to_contents '(:bool (:optional false))
                      :apply_recursively '(:bool (:optional false))
                      :delete_untracked  '(:bool (:optional false)) }))
+
+(defn try-json
+    "Given a string, tries to parse it as json. Returns empty map if failure"
+    [json-string]
+    (let [parse-result (try (parse-string json-string true) (catch java.lang.Exception e {}))]
+        (if (map? parse-result) parse-result {})))
+
+(defn string-key
+    "Given a map under construction and a key/value pair, adds the key/value pair to the map
+    but with the key as a string if it was a keyword previously"
+    [json-map keyval]
+    (let [oldk (first keyval)
+          oldv (val keyval)
+          k    (if (string? oldk) oldk (name oldk)) ]
+        (assoc json-map k oldv)))
+
+(defn parse-meta
+    "Given a string (presumably from a _meta.json file) parses it as best as possible as
+    into a metadata object. All top level keys are turned into strings. The values of those
+    keys are NOT massaged"
+    [meta-json]
+    (->> meta-json
+        (try-json)
+        (reduce string-key {})))
