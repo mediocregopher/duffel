b82a386f84a76e29913481fd004a5fb83a20dd5a
mediocregopher <bgp2396@bellsouth.net>
2012-12-16 17:54:23 -0500

    upped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duffel "0.6"
+(defproject duffel "0.7"
   :description "Duffel is a standalone executable which will automatically deploy a directory structure to your machine"
   :url "https://github.com/mediocregopher/duffel"
   :license {:name "Eclipse Public License"
