2bd9c5dd2ded9f525db36db11310b4a7cd7ef4b6
Brian Picciano <brian.picciano@escapemg.com>
2013-03-02 17:51:00 -0500

    made scripts directory be optional (like it should be)

--- src/duffel/script.clj
+++ src/duffel/script.clj
@@ -3,10 +3,15 @@
     (:require [duffel.fs-util :as dfs-util]))
 
 (defn exec-script
+    "Runs root-dir/_SCRIPTS/script-name with args. Returns nil if script doesn't
+    actually exist, otherwise returns output from the execution"
     [root-dir script-name & args]
-    (apply dfs-util/exec-in root-dir (str "_SCRIPTS/" script-name) args))
+    (let [script-rel-path (str "_SCRIPTS/" script-name)]
+        (when (dfs-util/exists? (str root-dir "/" script-rel-path))
+            (apply dfs-util/exec-in root-dir script-rel-path args))))
 
 (defn is-in-group?
     [root-dir group-name]
-    (= "1" (trim
-        (exec-script root-dir "is-in-group" group-name))))
+    (if-let [ret (exec-script root-dir "is-in-group" group-name)]
+        (= "1" (trim ret))
+        false))
