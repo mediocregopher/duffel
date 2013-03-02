feebb3df8551fd281475c7374e2e26b72a1063dd
Brian Picciano <brian.picciano@escapemg.com>
2013-03-02 17:52:30 -0500

    updated to 0.14, made duffel use clojure 1.5.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject duffel "0.13"
+(defproject duffel "0.14"
   :description "Duffel is a standalone executable which will automatically deploy a directory structure to your machine"
   :url "https://github.com/mediocregopher/duffel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojars.jhowarth/clojure-contrib "1.2.0-RC3"]
                  [org.clojars.mediocregopher/massage "0.1.8"]
                  [cheshire "5.0.1"]]
