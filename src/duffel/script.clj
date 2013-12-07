cf2d0b40ab20fe4de19d9be4e07734915682e9f8
Brian Picciano <bgp2396@bellsouth.net>
2013-12-07 15:17:14 -0500

    updated scripts

--- src/duffel/script.clj
+++ src/duffel/script.clj
@@ -1,17 +1,18 @@
 (ns duffel.script
     (:use     [clojure.string :only [trim]])
-    (:require [duffel.fs-util :as dfs-util]))
+    (:require [duffel.fs :refer [exists? exec-in]]))
 
 (defn exec-script
-    "Runs root-dir/_SCRIPTS/script-name with args. Returns nil if script doesn't
-    actually exist, otherwise returns output from the execution"
-    [root-dir script-name & args]
-    (let [script-rel-path (str "_SCRIPTS/" script-name)]
-        (when (dfs-util/exists? (str root-dir "/" script-rel-path))
-            (apply dfs-util/exec-in root-dir script-rel-path args))))
+  "Runs root-dir/_SCRIPTS/script-name with args. Returns nil if script doesn't
+  actually exist, otherwise returns output from the execution"
+  [app script-name & args]
+  (let [script-rel-path (str (app :proj-root) "scripts/" script-name)]
+    (when (exists? script-rel-path)
+      (apply exec-in (app :proj-root) script-rel-path args))))
 
 (defn is-in-group?
-    [root-dir group-name]
-    (if-let [ret (exec-script root-dir "is-in-group" group-name)]
-        (= "1" (trim ret))
-        false))
+  [app group-name]
+  "Returns whether or not this node is in the given group"
+  (if-let [ret (exec-script app "is-in-group" group-name)]
+    (= "1" (trim ret))
+    false))
