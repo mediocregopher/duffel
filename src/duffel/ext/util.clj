1f9b847c125b2deca0c041b87670abec72c93213
Brian Picciano <bgp2396@bellsouth.net>
2013-12-08 13:13:52 -0500

    fixed spacing in backup code

--- src/duffel/ext/util.clj
+++ src/duffel/ext/util.clj
@@ -63,9 +63,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn- setup-backup-dir
-    "Sets up (creates) backup directory"
-    [backup]
-    (dfs/mkdir-p backup))
+  "Sets up (creates) backup directory"
+  [backup]
+  (dfs/mkdir-p backup))
 
 (defn- remove-trailing-slash
   "Given a string, if it ends in a slash, removes it"
@@ -75,50 +75,51 @@
       dir-name))
 
 (defn- prepend-backup
-    [abs backup]
-    (str (remove-trailing-slash backup) abs))
+  [abs backup]
+  (str (remove-trailing-slash backup) abs))
 
 (defn- like-filename
-    [filename pot-filename]
-    (when (re-find (re-pattern (str filename "\\.[0-9]+$")) pot-filename) true))
+  [filename pot-filename]
+  (when (re-find (re-pattern (str filename "\\.[0-9]+$")) pot-filename) true))
 
 (defn- append-timestamp
-    [filename]
-    (str filename "." (int (/ (System/currentTimeMillis) 1000))))
+  [filename]
+  (str filename "." (int (/ (System/currentTimeMillis) 1000))))
 
 (defn- ls-backups
-    [full-backup-dir filename]
-    (->> full-backup-dir
-         (dfs/ls)
-         (filter (partial like-filename filename))))
+  [full-backup-dir filename]
+  (->> full-backup-dir
+       (dfs/ls)
+       (filter (partial like-filename filename))))
 
 (defn- prepare-backup
-    "Given an absolute directory path to a file (assumes ends in slash), a
-    filename, the path to the backup root, and the number of files allowed to be
-    backed up, creates the backup directory for the given file and makes sure
-    there's no more then backup-count-1 versions of the file already backed up"
-    [abs-dir filename backup backup-count]
-    (let [ full-backup-dir  (prepend-backup abs-dir backup)
-           backup-filenames (ls-backups full-backup-dir filename) ]
-        (when (> backup-count 0) (dfs/mkdir-p full-backup-dir))
-        (->> backup-filenames
-             (sort #(compare %2 %1))
-             (drop (dec backup-count))
-             (map #(str full-backup-dir %))
-             (map dfs/rm-rf))))
+  "Given an absolute directory path to a file (assumes ends in slash), a
+  filename, the path to the backup root, and the number of files allowed to be
+  backed up, creates the backup directory for the given file and makes sure
+  there's no more then backup-count-1 versions of the file already backed up"
+  [abs-dir filename backup backup-count]
+  (let [ full-backup-dir  (prepend-backup abs-dir backup)
+         backup-filenames (ls-backups full-backup-dir filename) ]
+    (when (> backup-count 0) (dfs/mkdir-p full-backup-dir))
+    (->> backup-filenames
+         (sort #(compare %2 %1))
+         (drop (dec backup-count))
+         (map #(str full-backup-dir %))
+         (map dfs/rm-rf))))
 
 (defn- backup-file
-    "Given an absolute directory path to a file (assumes ends in slash), a
-    filename, the path to the backup root, and the number of files allowed to be
-    backed up, creates a backup file while at the same time rotating out old
-    files"
-    [abs-dir filename backup-dir backup-count]
-    (setup-backup-dir backup-dir)
-    (doall (prepare-backup abs-dir filename backup-dir backup-count))
-    (when (and (> backup-count 0) (dfs/exists? (str abs-dir filename)))
-        (dfs/cp (str abs-dir filename)
-                     (str (prepend-backup abs-dir backup-dir)
-                          (append-timestamp filename)))))
+  "Given an absolute directory path to a file (assumes ends in slash), a
+  filename, the path to the backup root, and the number of files allowed to be
+  backed up, creates a backup file while at the same time rotating out old
+  files"
+  [abs-dir filename backup-dir backup-count]
+  (setup-backup-dir backup-dir)
+  (doall (prepare-backup abs-dir filename backup-dir backup-count))
+  (when (and (> backup-count 0) (dfs/exists? (str abs-dir filename)))
+    (dfs/cp
+      (str abs-dir filename)
+      (str (prepend-backup abs-dir backup-dir)
+           (append-timestamp filename)))))
 
 (defn try-backup
   "Attempts to backup the file at the given absolute path, using the app's
